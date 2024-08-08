508ac504dede2099a8205e2813ab6285410e37dc
IntegraMOD <john@jwinstallations.com>
2024-08-08 14:57:05 -0700

    Update install.php

--- phpBB/install/install.php
+++ phpBB/install/install.php
@@ -229,66 +229,62 @@ error_reporting  (E_ERROR | E_WARNING | E_PARSE); // This will NOT report uninit
 // Slash data if it isn't slashed
 if (true)
 {
-   if (is_array($_GET))
-   {
-       foreach((Array) $_GET as list ($k => $v)) 
-	   {
-         
-           if (is_array($_GET[$k]))
-           {
-		       foreach((Array) $_GET[$k] as list ($k2 => $v2)) 
-			   {
-			       $_GET[$k][$k2] = addslashes($v2);
-			   }
-				   @reset($_GET[$k]);
-           }
-           else
-           {
-               $_GET[$k] = addslashes($v);
-           }
-       }
-       @reset($_GET);
-   }
+	if (is_array($_GET)) {
+		foreach ($_GET as $k => $v) 
+		{
+			if (is_array($_GET[$k])) 
+			{
+				foreach ($_GET[$k] as $k2 => $v2) 
+				{
+					$_GET[$k][$k2] = addslashes($v2);
+				}
+				@reset($_GET[$k]);
+			} else {
+				$_GET[$k] = addslashes($v);
+			}
+		}
+		@reset($_GET);
+	}
 
-   if (is_array($_POST))
-   {
-      foreach((Array) $_POST as list ($k => $v)) 
-	  {
-         if (is_array($_POST[$k]))
-         {
-             foreach((Array) $_POST[$k] as list ($k2 => $v2)) 
-			 {
-                 $_POST[$k][$k2] = addslashes($v2);
-             }
-             @reset($_POST[$k]);
-         }
-         else
-         {
-             $_POST[$k] = addslashes($v);
-         }
-      }
-      @reset($_POST);
-   }
+	if (is_array($_POST))
+	{
+		foreach ($_POST as $k => $v) 
+		{
+			if (is_array($_POST[$k]))
+			{
+				foreach ($_POST[$k] as $k2 => $v2) 
+				{
+					$_POST[$k][$k2] = addslashes($v2);
+				}
+				@reset($_POST[$k]);
+			}
+			else
+			{
+				$_POST[$k] = addslashes($v);
+			}
+		}
+		@reset($_POST);
+	}
 
-   if (is_array($_COOKIE))
-   {
-      foreach((Array) $_COOKIE as list ($k => $v)) 
-	  {
-          if (is_array($_COOKIE[$k]))
-          {
-              foreach((Array) $_COOKIE[$k] as list ($k2 => $v2)) 
-			  {
-                  $_COOKIE[$k][$k2] = addslashes($v2);
-              }
-              @reset($_COOKIE[$k]);
-          }
-          else
-          {
-            $_COOKIE[$k] = addslashes($v);
-          }
-      }
-      @reset($_COOKIE);
-   }
+	if (is_array($_COOKIE))
+	{
+		foreach ($_COOKIE as $k => $v) 
+		{
+			if (is_array($_COOKIE[$k]))
+			{
+				foreach ($_COOKIE[$k] as $k2 => $v2) 
+				{
+					$_COOKIE[$k][$k2] = addslashes($v2);
+				}
+				@reset($_COOKIE[$k]);
+			}
+			else
+			{
+				$_COOKIE[$k] = addslashes($v);
+			}
+		}
+		@reset($_COOKIE);
+	}
 }
 
 // Begin main prog
