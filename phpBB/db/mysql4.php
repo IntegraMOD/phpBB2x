1fd4a60a4e810f7fea856e3e46f603ded505ffdb
Ray <2566493+MWE001@users.noreply.github.com>
2024-11-17 09:09:15 -0600

    Update mysql4.php
    Write space before opening tag removed

--- phpBB/db/mysql4.php
+++ phpBB/db/mysql4.php
@@ -1,23 +1,22 @@
-	<?php
-	/***************************************************************************
-	 *                                 mysql4.php
-	 *                            -------------------
-	 *   begin                : Saturday, Feb 13, 2001
-	 *   copyright            : (C) 2001 The phpBB Group
-	 *   email                : supportphpbb.com
-	 *
-	 *   $Id$
-	 *
-	 ***************************************************************************/
-	 
-	/***************************************************************************
-	 *
-	 *   This program is free software; you can redistribute it and/or modify
-	 *   it under the terms of the GNU General Public License as published by
-	 *   the Free Software Foundation; either version 2 of the License, or
-	 *   (at your option) any later version.
-	 *
-	 ***************************************************************************/
+<?php
+/***************************************************************************
+*                                 mysql4.php
+*                            -------------------
+*   begin                : Saturday, Feb 13, 2001
+*   copyright            : (C) 2001 The phpBB Group
+*   email                : supportphpbb.com
+*
+*   $Id$
+*
+***************************************************************************/
+/***************************************************************************
+*
+*   This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.
+*
+***************************************************************************/
 	 
 	if(!defined("SQL_LAYER"))
 	{
